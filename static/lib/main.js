2086bf26fa00637cef60947ed779148387b8f393
Davis Voicescuks <davis.voicescuks@gmail.com>
2018-04-20 22:14:40 +0300

    Load missing styles in main.

--- static/lib/main.js
+++ static/lib/main.js
@@ -1,7 +1,6 @@
-
-'use strict';
+"use strict";
 /* global ajaxify, app, $ */
-(function () {
+(function() {
   /*
     This file shows how client-side javascript can be included via a plugin.
     If you check `plugin.json`, you"ll see that this file is listed under "scripts".
@@ -11,47 +10,58 @@
     $(document).ready(); Fired when the DOM is ready
     $(window).on("action:ajaxify.end", function(data) { ... }); "data" contains "url"
   */
-  var validUrls = ['/landing', '/roster', '/apply', '/applications']
-  var root = document.createElement('div')
-  root.setAttribute('id', 'moonlight-root')
-  require(['/plugins/nodebb-plugin-moonlight/public/main.js'], function (App) {
-    App.initSocket()
-  })
-  var matchRooms = function (path) {
+  var validUrls = ["/landing", "/roster", "/apply", "/applications"];
+  var link = document.createElement("link");
+  link.setAttribute("rel", "stylesheet");
+  link.setAttribute("type", "text/css");
+  link.setAttribute(
+    "href",
+    "/plugins/nodebb-plugin-moonlight/public/moonlight.css"
+  );
+  document.head.appendChild(link);
+  var root = document.createElement("div");
+  root.setAttribute("id", "moonlight-root");
+  require(["/plugins/nodebb-plugin-moonlight/public/main.js"], function(App) {
+    App.initSocket();
+  });
+  var matchRooms = function(path) {
     switch (path) {
-      case '/landing':
-        return 'mnl_landing'
-      case '/roster':
-        return 'mnl_roster'
-      case '/apply':
-        return 'mnl_apply'
-      case '/applications':
-        return 'mnl_applications'
-      case '/application':
-        return 'mnl_application'
+      case "/landing":
+        return "mnl_landing";
+      case "/roster":
+        return "mnl_roster";
+      case "/apply":
+        return "mnl_apply";
+      case "/applications":
+        return "mnl_applications";
+      case "/application":
+        return "mnl_application";
       default:
-        return 'mnl'
+        return "mnl";
     }
-  }
+  };
 
-  $(window).on('action:ajaxify.contentLoaded', function (data) {
-    require(['/plugins/nodebb-plugin-moonlight/public/main.js'], function (App) {
-      if (validUrls.indexOf(ajaxify.data.url) !== -1 || ajaxify.data.url.startsWith('/application')) {
-        App.render(root, ajaxify.data.url)
-        document.getElementById('moonlight-content').appendChild(root)
+  $(window).on("action:ajaxify.contentLoaded", function(data) {
+    require(["/plugins/nodebb-plugin-moonlight/public/main.js"], function(App) {
+      if (
+        validUrls.indexOf(ajaxify.data.url) !== -1 ||
+        ajaxify.data.url.startsWith("/application")
+      ) {
+        App.render(root, ajaxify.data.url);
+        document.getElementById("moonlight-content").appendChild(root);
         if (ajaxify.data.action) {
-          App.store.dispatch(ajaxify.data.action)
+          App.store.dispatch(ajaxify.data.action);
         } else if (ajaxify.data.actions) {
-          ajaxify.data.actions.forEach(function (action) {
-            App.store.dispatch(action)
-          })
+          ajaxify.data.actions.forEach(function(action) {
+            App.store.dispatch(action);
+          });
         }
-        var url = ajaxify.data.url.replace('loggedin', '')
-        if (url.endsWith('?')) {
-          url.slice(0, -1)
+        var url = ajaxify.data.url.replace("loggedin", "");
+        if (url.endsWith("?")) {
+          url.slice(0, -1);
         }
-        app.enterRoom(matchRooms(ajaxify.data.url), function () { })
+        app.enterRoom(matchRooms(ajaxify.data.url), function() {});
       }
-    })
-  })
-}())
+    });
+  });
+})();
