689107d58e5df840dfab8f05e00503504678495c
Davis Voicescuks <davis.voicescuks@gmail.com>
2018-04-20 22:19:00 +0300

    Revert webpack config changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,40 @@
-const path = require("path");
-const webpack = require("webpack");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   context: __dirname,
   entry: {
-    moonlight: ["./src/client/main.scss"],
-    admin: ["./src/client/admin.scss"],
-    main: ["./src/client/index.tsx"]
+    moonlight: [
+      './src/client/main.scss'
+    ],
+    admin: [
+      './src/client/admin.scss'
+    ],
+    main: [
+      './src/client/index.tsx'
+    ]
   },
   output: {
-    path: path.join(__dirname, "dist"),
-    libraryTarget: "amd",
-    library: "/plugins/nodebb-plugin-moonlight/public/[name].js",
-    filename: "[name].js?",
-    sourceMapFilename: "[name].map?"
+    path: path.join(__dirname, 'dist'),
+    libraryTarget: 'amd',
+    filename: '[name].js?',
+    sourceMapFilename: '[name].map?'
   },
 
   // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  devtool: 'source-map',
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+    extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
     // https://webpack.js.org/configuration/resolve/#resolve-alias
-    alias: {},
-    modules: ["node_modules", path.resolve(__dirname, "node_modules")]
+    alias: {
+    },
+    modules: [
+      'node_modules',
+      path.resolve(__dirname, 'node_modules')
+    ]
   },
 
   module: {
@@ -33,21 +42,20 @@ module.exports = {
       // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
       {
         test: /\.tsx?$/,
-        loader: "ts-loader"
-      },
-      {
+        loader: 'ts-loader'
+      }, {
         test: /(\.scss|\.css)$/,
         loader: ExtractTextPlugin.extract({
-          fallback: "style-loader",
+          fallback: 'style-loader',
           use: [
             {
-              loader: "css-loader",
+              loader: 'css-loader',
               options: {
                 sourceMaps: true
               }
             },
             {
-              loader: "sass-loader",
+              loader: 'sass-loader',
               options: {
                 sourceMaps: true
               }
@@ -59,15 +67,15 @@ module.exports = {
         test: /\.(jpe?g|png|gif|svg)$/i,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              hash: "sha512",
-              digest: "hex",
-              name: "[hash].[ext]"
+              hash: 'sha512',
+              digest: 'hex',
+              name: '[hash].[ext]'
             }
           },
           {
-            loader: "image-webpack-loader",
+            loader: 'image-webpack-loader',
             options: {
               bypassOnDebug: true,
               optipng: {
@@ -83,21 +91,19 @@ module.exports = {
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {
         test: /\.js$/,
-        enforce: "pre",
-        loader: "source-map-loader"
+        enforce: 'pre',
+        loader: 'source-map-loader'
       }
     ]
   },
   plugins: [
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(
-        process.env.NODE_ENV || "development"
-      )
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     }),
     new ExtractTextPlugin({
-      filename: "[name].css",
+      filename: '[name].css',
       disable: false,
       allChunks: true
     })
   ]
-};
+}
